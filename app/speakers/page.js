bbc92c7495c4c636c898e2267f007dc10fa20da0
yosef-tutian <yoseftutian@gmail.com>
2024-04-01 09:51:16 +0300

    Update page.js

--- app/speakers/page.js
+++ app/speakers/page.js
@@ -2,7 +2,7 @@ import ProductsGrid from "@/utils/components/productsGrid/productsGrid";
 import { getAllSpeakers } from "@/utils/functions/apiCalls";
 
 export default async function SpeakersPage() {
-  const speakers = await getAllSpeakers();
+  // const speakers = await getAllSpeakers();
   return (
     <div>
       <h1>speakers</h1>
