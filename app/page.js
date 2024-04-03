d15753d7279c391c4fd5eabed17fab620190a784
yosef-tutian <yoseftutian@gmail.com>
2024-04-03 19:44:06 +0300

    added todo

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import Todo from "@/utils/components/todo/todo";
 import "./page.css";
 
 export default function Home() {
-  return <div className="main">hello</div>;
+  return (
+    <div className="main">
+      <Todo />
+    </div>
+  );
 }
---
+++ utils/components/todo/todo.js
@@ -0,0 +1,78 @@
+"use client";
+import { Add, CheckBox, Delete } from "@mui/icons-material";
+import { Checkbox, IconButton, TextField } from "@mui/material";
+import { nanoid } from "nanoid";
+import { useRef, useState } from "react";
+
+export default function Todo() {
+  const [todos, setTodos] = useState([]);
+  const titleRef = useRef(null);
+  function addTodo() {
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: nanoid(),
+        title: titleRef.current.value,
+        finished: false,
+        date: new Date(),
+      },
+    ]);
+    titleRef.current.value = "";
+  }
+  return (
+    <div className="column center" style={{ height: "85vh" }}>
+      <div className="row" style={{ width: "400px", gap: "20px" }}>
+        <TextField inputRef={titleRef} />
+        <IconButton onClick={addTodo}>
+          <Add />
+        </IconButton>
+      </div>
+      <table style={{ width: "400px" }}>
+        {todos.map((todo) => (
+          <TodoItem key={nanoid()} todo={todo} setTodos={setTodos} />
+        ))}
+      </table>
+    </div>
+  );
+}
+
+function TodoItem({ todo, setTodos }) {
+  return (
+    <tr
+      style={{
+        width: "100%",
+      }}
+    >
+      <td>
+        <h2>{todo.title}</h2>
+      </td>
+
+      <td>
+        <input
+          type="checkbox"
+          checked={todo.finished}
+          onChange={() => {
+            setTodos((prev) =>
+              prev.map((p) => {
+                if (p.id === todo.id) return { ...p, finished: !p.finished };
+                return p;
+              })
+            );
+          }}
+        />
+      </td>
+      <td>
+        <IconButton
+          onClick={() =>
+            setTodos((prev) => prev.filter((p) => p.id !== todo.id))
+          }
+        >
+          <Delete />
+        </IconButton>
+      </td>
+      <td>
+        <p>{todo.date.toString()}</p>
+      </td>
+    </tr>
+  );
+}
