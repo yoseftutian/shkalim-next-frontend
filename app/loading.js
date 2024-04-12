67a2642d24b6994908849397af38991eebef058e
yosef-tutian <yoseftutian@gmail.com>
2024-04-12 17:56:38 +0300

    added skeleton

--- app/loading.js
+++ app/loading.js
@@ -1,9 +1,21 @@
-import { CircularProgress } from "@mui/material";
+"use client";
+import { usePathname } from "next/navigation";
 import "./loading.css";
+import TodoSkeleton from "@/utils/components/todo/todoSkeleton";
 export default function Loading() {
+  const pathname = usePathname();
   return (
     <div className="column center middleware-height">
-      <CircularProgress />
+      {getSkeletonByPage(pathname)}
     </div>
   );
+}
+
+function getSkeletonByPage(pathname) {
+  switch (pathname) {
+    case "/":
+      return <TodoSkeleton />;
+    default:
+      return <TodoSkeleton />;
+  }
 }
